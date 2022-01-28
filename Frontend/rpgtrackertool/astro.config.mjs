d882ba03c2b47eaddf3a4bfe53b7c7ef11bbd637
mikechurvis <mikechurvis@gmail.com>
2022-01-28 13:47:48 -0500

    Setup Svelte & Typescript

--- Frontend/rpgtrackertool/astro.config.mjs
+++ Frontend/rpgtrackertool/astro.config.mjs
@@ -1,5 +1,13 @@
+// @ts-check
 export default /** @type {import('astro').AstroUserConfig} */ ({
   renderers: [
     '@astrojs/renderer-svelte',
   ],
+  packageOptions: {
+    types: true,
+  },
+  buildOptions: {
+    out: "dist",
+    sourcemap: true,
+  },
 });
---
+++ Frontend/rpgtrackertool/src/components/TestComponent.svelte
@@ -0,0 +1,7 @@
+<script>
+  let value = "";
+</script>
+
+<h2>Hello from Svelte!</h2>
+<input type="text" bind:value={value}>
+<p>^that says {value}</p>
